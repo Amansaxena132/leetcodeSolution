4d9f43b8cefe78dc849fb11b6f5c77141f080bf1
Amansaxena132 <42863454+Amansaxena132@users.noreply.github.com>
2025-02-05 17:21:09 +0530

    Time: 0 ms (100%), Space: 41.5 MB (63.5%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -10,9 +10,6 @@ class Solution {
             while (!strs[i].startsWith(prefix)) {
                 prefix = prefix.substring(0, prefix.length() - 1);
 
-                if (prefix.isEmpty()) {
-                    return "";
-                }
             }
         }
 
